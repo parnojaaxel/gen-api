e2f76e04c59dc834879af8a362f563b057815d5f
parnojaaxel <113966806+parnojaaxel@users.noreply.github.com>
2023-10-24 13:33:58 +0300

    apo

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,180 @@
-import Image from 'next/image'
+"use client"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import React, { useState, useEffect } from 'react';
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Recipe {
+  id: number;
+  title: string;
+  body: string;
 }
+
+const RecipesPage: React.FC = () => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const [editingRecipe, setEditingRecipe] = useState<Recipe | null>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const apiUrl = 'https://usman-fake-api.herokuapp.com/api/recipes';
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await fetch(apiUrl);
+      if (response.ok) {
+        const data = await response.json();
+        setRecipes(data);
+      } else {
+        console.error('Ei õnnestunud retsepte tuua');
+      }
+    } catch (error) {
+      console.error('Tekkis viga:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const createRecipe = async () => {
+    const newRecipe: Recipe = {
+      id: 0,
+      title: 'Uus retsept',
+      body: 'Uue retsepti kirjeldus',
+    };
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newRecipe),
+      });
+      if (response.ok) {
+        fetchRecipes();
+      } else {
+        console.error('Ei õnnestunud uut retsepti luua');
+      }
+    } catch (error) {
+      console.error('Tekkis viga:', error);
+    }
+  };
+
+  const updateRecipe = async (recipeId: number) => {
+    const updateUrl = `${apiUrl}/${recipeId}`;
+    const updatedRecipe: Recipe = {
+      id: recipeId,
+      title: editingRecipe?.title || '',
+      body: editingRecipe?.body || '',
+    };
+
+    try {
+      const response = await fetch(updateUrl, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedRecipe),
+      });
+      if (response.ok) {
+        fetchRecipes();
+      } else {
+        console.error('Ei õnnestunud retsepti uuendada');
+      }
+    } catch (error) {
+      console.error('Tekkis viga:', error);
+    }
+  };
+
+  const deleteRecipe = async (recipeId: number) => {
+    const deleteUrl = `${apiUrl}/${recipeId}`;
+
+    try {
+      const response = await fetch(deleteUrl, {
+        method: 'DELETE',
+      });
+
+      if (response.ok) {
+        fetchRecipes();
+      } else {
+        console.error('Ei õnnestunud retseptit kustutada');
+      }
+    } catch (error) {
+      console.error('Tekkis viga:', error);
+    }
+  };
+
+  const handleEdit = (recipe: Recipe) => {
+    setEditingRecipe(recipe);
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    if (editingRecipe) {
+      updateRecipe(editingRecipe.id);
+      setEditingRecipe(null);
+      setIsEditing(false);
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-semibold mb-4">Retseptide nimekiri</h1>
+      <div className="mb-4">
+        <button onClick={createRecipe} className="bg-blue-500 text-white px-3 py-2 rounded mr-4">
+          Loo
+        </button>
+      </div>
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {recipes.map((recipe) => (
+          <li key={recipe.id} className="bg-white p-4 rounded shadow-md flex flex-col">
+            {editingRecipe === recipe ? (
+              <div>
+                <input
+                  type="text"
+                  value={editingRecipe?.title || ''}
+                  onChange={(e) =>
+                    setEditingRecipe({ ...editingRecipe, title: e.target.value })
+                  }
+                  className="mb-2 px-2 py-1 rounded text-black"
+                />
+                <input
+                  type="text"
+                  value={editingRecipe?.body || ''}
+                  onChange={(e) =>
+                    setEditingRecipe({ ...editingRecipe, body: e.target.value })
+                  }
+                  className="mb-2 px-2 py-1 rounded text-black"
+                />
+              </div>
+            ) : (
+              <div>
+                <h3 className="text-lg font-semibold mb-2 text-black">{recipe.title}</h3>
+                <p className="text-gray-500 mb-2">{recipe.body}</p>
+              </div>
+            )}
+            <div>
+              <button
+                onClick={() => {
+                  if (editingRecipe === recipe) {
+                    handleSave();
+                  } else {
+                    handleEdit(recipe);
+                  }
+                }}
+                className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 mb-2"
+              >
+                {editingRecipe === recipe ? 'Salvesta' : 'Muuda'}
+              </button>
+              <button
+                onClick={() => deleteRecipe(recipe.id)}
+                className="bg-red-500 text-white px-3 py-1 rounded hover-bg-red-600"
+              >
+                Kustuta
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecipesPage;
